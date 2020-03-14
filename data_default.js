01bf84e2bd68b5ac06fae856a141383e4369cbb6
Muhammad Idil Haq Amir <idilhaq08@gmail.com>
2020-03-14 10:56:27 +0700

    Data

--- data_default.js
+++ data_default.js
@@ -7,17 +7,17 @@ a[1].innerHTML = date;
 //Dashboard
 //Positif
 document.getElementById("positif").innerHTML = "69";
-document.getElementById("positif_note").innerHTML = "35";
+document.getElementById("positif_note").innerHTML = "0";
 //Meninggal
 document.getElementById("meninggal").innerHTML = "4";
 document.getElementById("meninggal_note").innerHTML = "0";
 //Sembuh
 document.getElementById("sembuh").innerHTML = "5";
-document.getElementById("sembuh_note").innerHTML = "2";
+document.getElementById("sembuh_note").innerHTML = "0";
 //Proses Pemeriksaan
 document.getElementById("pemeriksaan").innerHTML = "0";
 //Negatif
 document.getElementById("negatif").innerHTML = "917";
-document.getElementById("negatif_note").innerHTML = "106";
+document.getElementById("negatif_note").innerHTML = "0";
 //Total Test
 document.getElementById("totaltes").innerHTML = "986";
