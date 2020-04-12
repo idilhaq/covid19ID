b55b01a6a03b634023c94281b102de07e615d851
Muhammad Idil Haq Amir <idilhaq08@gmail.com>
2020-04-12 16:49:21 +0700

    Update Minggu

--- data_default.js
+++ data_default.js
@@ -4,8 +4,8 @@ function numberWithCommas(x) {
 
 //Tanggal Update
 var a = document.getElementsByClassName("tgl_update");
-var date = 'Update per Sabtu, 11 April 2020 16:43 WIB';
-var x_data = [3842,330,327,21,286,4,0,15940,1554,19452,9.,8.51];
+var date = 'Update per Minggu, 12 April 2020 16:46 WIB';
+var x_data = [4241,399,373,46,359,73,0,16122,182,19964,10,8.79];
 // positif         = x_data[0]
 // new_positif     = x_data[1]
 // meninggal       = x_data[2]
