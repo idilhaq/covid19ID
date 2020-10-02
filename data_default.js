1adb4dd54310d0d50cf82eadf671ee6563020eb0
Muhammad Idil Haq Amir <idilhaq08@gmail.com>
2020-10-02 18:13:28 +0700

    Update Jumat

--- data_default.js
+++ data_default.js
@@ -7,8 +7,8 @@ var a = document.getElementsByClassName("tgl_update");
 var b = document.getElementsByClassName("spec_msg");
 var spec_msg = '#12WeekToGoFor2021';
 
-var date = 'Update per Kamis, 01 October 2020 19:02 WIB'
-var x_data = [291182,4174,10856,116,218487,3540,,1732808,26122,2023990,1.4,3.72,30296];
+var date = 'Update per Jumat, 02 October 2020 18:11 WIB'
+var x_data = [295499,4317,10972,116,221340,2853,,1755322,22514,2050821,1.4,3.71,26831];
 
 a[0].innerHTML = a[1].innerHTML = date;
 b[0].innerHTML = spec_msg;
