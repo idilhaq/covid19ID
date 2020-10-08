b7b422b974450c12146102698b78a0291caa5ead
idil@skbf.co.id <idil@skbf.ad>
2020-10-08 16:23:34 +0700

    Update Demo, Lanjutkan!

--- data_default.js
+++ data_default.js
@@ -5,10 +5,10 @@ function numberWithCommas(x) {
 //Tanggal Update
 var a = document.getElementsByClassName("tgl_update");
 var b = document.getElementsByClassName("spec_msg");
-var spec_msg = '#TetapJagaKesehatanSaatBerdemo';
+var spec_msg = '#5KDailyCasesIncoming';
 
-var date = 'Update per Rabu, 07 October 2020 21:00 WIB'
-var x_data = [315714,4538,11472,98,240291,3854,,1861961,27629,2177675,1.4,3.63,32167];
+var date = 'Update per Kamis, 08 October 2020 16:02 WIB'
+var x_data = [320564,4850,11580,108,244060,3769,,1890012,28051,2210576,1.5,3.61,32901];
 
 a[0].innerHTML = a[1].innerHTML = date;
 b[0].innerHTML = spec_msg;
